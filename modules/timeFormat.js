dcbec527ddcc64b767d7ba5ce7a697f7d0450051
Bartek Buslowicz <bartek@burin.pl>
2018-01-16 17:36:44 +0100

    Add ready project

---
+++ modules/timeFormat.js
@@ -0,0 +1,12 @@
+var colors = require('colors/safe');
+function timeFormat() {
+	var os = require('os');
+	var uptime = os.uptime().toFixed(0);
+	var  hours = (uptime/3600);
+	var onlyHours = hours | 0;
+	var minutes = ((uptime % 3600) / 60);
+	var onlyMinutes = minutes | 0;
+	var seconds = (uptime - ((onlyHours * 3600) + (onlyMinutes * 60)));
+	console.log(colors.green('Czas działania systemu: '), onlyHours,'h ', onlyMinutes,'min ', seconds,'s.');
+}
+exports.print2 = timeFormat;
